7688b1b3fc911b3a00dc4a2030d9dd8f87210d8f
joaquindiazs <joaquindiazschillagi@gmail.com>
2019-08-03 09:27:18 -0300

    Add rspec-its gem and complete scenarios to make movements for ttt

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 
 gem "rspec", "~> 3.8.0"
+gem "rspec-its"
+
 gem "guard", "~> 2.15.0"
 gem "guard-rspec", "~> 4.7.3"
 gem "gosu", "~> 0.14.5"
--- lib/position.rb
+++ lib/position.rb
@@ -1,9 +1,49 @@
 class Position
+  # getter/setter for :board, :turn
   attr_accessor :board, :turn
+
   DIM = 3
   SIZE = DIM * DIM
-  def initialize
-    @board = %w(-)*SIZE
-    @turn = "x"
+  def initialize(board=nil, turn="x")
+    @board = board || %w(-)*SIZE
+    @turn = turn
+  end
+
+  def move(idx)
+    position = Position.new(@board.dup, xturn("o", "x"))
+    position.board[idx] = turn
+    position
+  end
+
+  def xturn(x,o)
+    turn == "x" ? x : o
+  end
+
+  def possible_moves
+    @board.map.with_index { |p,i| i if p =="-" }.compact
+  end
+
+  def win?(turn)
+    rows = @board.each_slice(DIM).to_a
+    rows.any? { |row| row.all? { |p| p == turn } } ||
+    rows.transpose.any? { |col| col.all? { |p| p == turn } } ||
+    rows.map.with_index.all? { |row,i| row[i] == turn } ||
+    rows.map.with_index.all? { |row,i| row[DIM-1-i] == turn }
+  end
+
+  def minimax(depth=1)
+    return 100 if win?("x")
+    return -100 if win?("o")
+    return 0 if possible_moves.empty?
+    @@minimax ||= {}
+    value = @@minimax[@board]
+    return value if value
+    @@minimax[@board] = possible_moves.map { |idx|
+      move(idx).minimax(depth+1) 
+    }.send(xturn(:max, :min)) + xturn(-depth,depth)
+  end
+
+  def best_move
+    possible_moves.send(xturn(:max_by, :min_by)) { |idx| move(idx).minimax }
   end
 end
